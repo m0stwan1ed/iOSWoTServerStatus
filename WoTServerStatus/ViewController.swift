147737890212ff41824e904328099b143fd48ce7
Vladyslav Mostovych <m0stwan1ed@gmail.com>
2020-01-11 08:27:00 -0800

    Initial Commit

---
+++ WoTServerStatus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoTServerStatus
+//
+//  Created by Wladyslaw Mostowicz on 11/01/2020.
+//  Copyright © 2020 Wladyslaw Mostowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
